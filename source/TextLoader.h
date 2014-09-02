9daf893958df19fe7398bfb7349d15a0b1694e10
fullripefruit <he@live.jp>
2014-09-02 18:38:29 +0900

    Modified comparison between char * and false

--- source/TextLoader.h
+++ source/TextLoader.h
@@ -29,7 +29,7 @@ namespace lyrics
 
 			text = TextLoader::ConvertToUTF16( ( unsigned char * )data, size, length );
 			delete [] data;
-			if ( text == false )
+			if ( text == nullptr )
 			{
 				// TODO:
 				return false;
